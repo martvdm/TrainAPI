2cf38492414688171fbcb42239a800b02155da3b
Martvdm <martvandermolen@gmail.com>
2022-04-01 18:22:16 +0200

    feat: test slashcommand

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
+from discord.ext import tasks, commands
+from discord.ext.commands import Bot
+from discord_slash import SlashCommand, SlashContext
 
-
-
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
+slash = SlashCommand(client)
 
 import json
 with open("config.json") as jsonfile:
@@ -11,7 +13,12 @@ with open("config.json") as jsonfile:
 @client.event
 async def on_ready():
     print('\033[92mLoading data... \n \033[94mLoaded client: {0.user}'.format(client))
+    print('\033[92mLoading commands...')
+    print('\033[92mLoaded commands!')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"ov-NL"))
 
-
+@slash.slash(name="test")
+async def test(ctx: SlashContext):
+    await ctx.send("test")
 
 client.run(config['token'])
