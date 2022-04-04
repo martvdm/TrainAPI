0f344862851551f8845103cc75f889e2e164b47e
Martvdm <martvandermolen@gmail.com>
2022-04-04 11:14:19 +0200

    feat: api controller

---
+++ __api__.py
@@ -0,0 +1,23 @@
+import json
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+
+def request_api_travelinfo(type, params):
+    with open("config.json") as jsonfile:
+        config = json.load(jsonfile)
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': f"{config['api']['NS-PRIMARY']}",
+    }
+    try:
+        conn = http.client.HTTPSConnection('gateway.apiportal.ns.nl')
+        conn.request("GET", f"/reisinformatie-api/api/v2/{type}?%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        json_raw = response.read()
+        json_data = json.loads(json_raw)
+        conn.close()
+    except Exception as e:
+        print('Het ophalen van de API is mislukt. Probeer het later opnieuw.')
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+    return json_data
