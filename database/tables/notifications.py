4ac8c60b32c91c2674fc98abffce294701dc8178
Martvdm <martvandermolen@gmail.com>
2022-04-22 22:13:42 +0200

    fix: table notifications

--- database/tables/notifications.py
+++ database/tables/notifications.py
@@ -2,25 +2,23 @@ import mysql
 
 def index(cursor):
     cursor.execute("DROP TABLE IF EXISTS NOTIFICATIONS")
-
     sql = '''CREATE TABLE NOTIFICATIONS(
     ID int AUTO_INCREMENT,
     CLIENT_ID VARCHAR(255) NOT NULL,
     STATION VARCHAR(5) NOT NULL,
     PRIMARY KEY (ID)
     )'''
-
     cursor.execute(sql)
 
 
-async def create(config, ctx, client_id, stationcode):
+def create(config, client_id, stationcode):
     import pandas as pd
     conn = mysql.connector.connect(user=config['database']['username'], password=config['database']['password'], host=config['database']['host'], port=config['database']['port'], database=config['database']['database'])
     cursor = conn.cursor()
     sql = "INSERT INTO NOTIFICATIONS (CLIENT_ID, STATION) VALUES (%s, %s)"
     statement = pd.read_sql(f"SELECT * FROM NOTIFICATIONS WHERE STATION = '{stationcode}' AND CLIENT_ID = '{client_id}'", conn)
     if statement.values.any():
-        await ctx.send(f"Client already has a notification set")
+        message = f"Client already has a notification set"
     else:
         cursor.execute(sql, (client_id, stationcode))
         conn.commit()
