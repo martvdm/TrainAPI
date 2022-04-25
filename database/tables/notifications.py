066f5cc77a7a97b46dcd026d8dd8e0e700bbb6b5
Martvdm <martvandermolen@gmail.com>
2022-04-25 16:22:06 +0200

    feat: Subscribe and unsubribe function

--- database/tables/notifications.py
+++ database/tables/notifications.py
@@ -11,31 +11,40 @@ def index(cursor):
     cursor.execute(sql)
 
 
-def create(config, client_id, stationcode):
+def create(config, action, client_id, station):
+    stationcode = station['stationCode']
     import pandas as pd
-    conn = mysql.connector.connect(user=config['database']['username'], password=config['database']['password'], host=config['database']['host'], port=config['database']['port'], database=config['database']['database'])
+    conn = mysql.connector.connect(user=config['database']['username'], password=config['database']['password'],
+                                   host=config['database']['host'], port=config['database']['port'],
+                                   database=config['database']['database'])
     cursor = conn.cursor()
-    sql = "INSERT INTO NOTIFICATIONS (CLIENT_ID, STATION) VALUES (%s, %s)"
-    statement = pd.read_sql(f"SELECT * FROM NOTIFICATIONS WHERE STATION = '{stationcode}' AND CLIENT_ID = '{client_id}'", conn)
-    if statement.values.any():
-        message = f"Client already has a notification set"
-    else:
-        cursor.execute(sql, (client_id, stationcode))
-        conn.commit()
-        message = f"Notification created for client {client_id} at station {stationcode}"
+    searchnotification = pd.read_sql(f"SELECT * FROM NOTIFICATIONS WHERE STATION = '{stationcode}' AND CLIENT_ID = '{client_id}'", conn)
+    if action == 'subscribe':
+        sql = "INSERT INTO NOTIFICATIONS (CLIENT_ID, STATION) VALUES (%s, %s)"
+        if searchnotification.values.any():
+            message = f"Client already has a notification set on station {station['name']}"
+        else:
+            cursor.execute(sql, (client_id, stationcode))
+            conn.commit()
+            message = f"Subscribed to notifications for station {station['name']}"
+    elif action == 'unsubscribe':
+        if searchnotification.values.any():
+            sql = f"DELETE FROM NOTIFICATIONS WHERE CLIENT_ID = '{client_id}' AND STATION = '{stationcode}'"
+            cursor.execute(sql)
+            conn.commit()
+            message = f"Unsubscribed from notifications for station {station['name']}"
+        else:
+            message = f"No notification found for station {station['name']}"
     conn.close()
     return message
 
-def find_users(config,  stationcode):
-    import warnings
+
+def find_users(config, stationcode):
     import pandas as pd
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', UserWarning)
-    conn = mysql.connector.connect(user=config['database']['username'], password=config['database']['password'], host=config['database']['host'], port=config['database']['port'], database=config['database']['database'])
-    cursor = conn.cursor()
+    conn = mysql.connector.connect(user=config['database']['username'], password=config['database']['password'],
+                                   host=config['database']['host'], port=config['database']['port'],
+                                   database=config['database']['database'])
     sql = f"SELECT CLIENT_ID FROM NOTIFICATIONS WHERE STATION = '{stationcode}'"
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', UserWarning)
-        users = pd.read_sql(sql, conn)
+    users = pd.read_sql(sql, conn)
     conn.close()
-    return users.values
+    return users.values.tolist()
