20c6945884a2e56083fd0efa66c896314d38e343
Martvdm <martvandermolen@gmail.com>
2022-04-25 16:18:40 +0200

    feat: create table disruptions

--- database/__init__.py
+++ database/__init__.py
@@ -5,7 +5,9 @@ def create_tables(config):
     conn = mysql.connector.connect(user=config['database']['username'], password=config['database']['password'], host=config['database']['host'], port=config['database']['port'], database=config['database']['database'])
     import database.tables.trips as trips
     import database.tables.notifications as notifications
+    import database.tables.disruptions as disruptions
     cursor = conn.cursor()
+    disruptions.index(cursor)
     trips.index(cursor)
     notifications.index(cursor)
     conn.close()
